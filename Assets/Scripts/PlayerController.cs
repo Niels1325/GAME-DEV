79042d30ac4c8e8fd3929155796dcac0e0eb6585
ShrilankaMeneer <110306001+ShrilankaMeneer@users.noreply.github.com>
2023-01-17 17:43:20 +0100

    Plane & starting basic movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Moving the player
+        transform.Translate(Vector3.down * Time.deltaTime * 20);
+    }
+}
