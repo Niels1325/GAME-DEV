5cfab9eb0b0ac1171587c0ef1cf7ffc039f23402
ShrilankaMeneer <nkloosterman@roc-dev.com>
2023-03-10 16:06:40 +0100

    Fixed shooting (working on healthpoints script and zombie doing damage)

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -9,6 +9,7 @@ public class Shooting : MonoBehaviour
     public GameObject bulletPrefab;
 
     public float bulletForce = 200f;
+    private Vector3 aim;
 
     public Rigidbody rb;
     // Start is called before the first frame update
@@ -23,12 +24,19 @@ public class Shooting : MonoBehaviour
         if(Input.GetButtonDown("Fire1")) {
             Shoot();
         }
+
+        Camera cam = Camera.main;
+ 
+        Vector3 mousePos = Input.mousePosition;
+        aim = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
     }
 
     void Shoot() 
     {
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        firePoint.transform.LookAt(aim);
+        var instance = Instantiate(bulletPrefab, firePoint.transform.position, firePoint.rotation);
+        instance.transform.LookAt(aim);
         //rb = GetComponent<Rigidbody>();
-        rb.AddForce(Vector3.up * bulletForce, ForceMode.Acceleration);
+        instance.GetComponent<Rigidbody>().AddRelativeForce(Vector3.up * bulletForce, ForceMode.Impulse);
     }
 }
