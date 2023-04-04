3f4a3559f7fe15649c5dbe9a16d69b07acdd7a77
Niels Kloosterman <70892664+Niels1325@users.noreply.github.com>
2023-04-04 13:51:30 +0200

    Fixed spawner en 10 zombies per ronde (nu bezig met einde van de game)

--- Assets/Scripts/ZombieAI.cs
+++ Assets/Scripts/ZombieAI.cs
@@ -5,18 +5,24 @@ using UnityEngine.AI;
 
 public class ZombieAI : MonoBehaviour
 {
-
+    //[SerializeField] private GameObject target;
     //Class voor NavMeshAgent (functie in Unity voor automatische beweging)
     NavMeshAgent nm;
 
+    private Transform target;
+
     //Class voor waar dit object (zombie) heen moet lopen (in dit geval target aka player).
-    public Transform target;
+    //public Transform target = Transform.FindGameObjectsWithTag("player");
+
+    //gameObject.tag = "player";
 
     // Start is called before the first frame update
     void Start()
     {
         //navmeshagent is de navmeshagent van de zombie.
         nm = GetComponent<NavMeshAgent>();
+        //Zet target op de player via deze tag
+        target = GameObject.FindWithTag("playertransform").transform;
     }
 
     // Update is called once per frame
--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -5,19 +5,33 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-
+    //Zombie prefab toevoegen zodat deze gespawned kan worden
     public GameObject zombiePrefab;
 
+    //Het aantal zombies dat per ronde spawned.
     public int amountOfZombiesPerRound = 10;
 
+    //Het tijd tussen rondes
     public float timeBetweenRounds = 10f;
 
-    private int waveNumber = 10;
+    //Welke ronde je zit.
+    private int waveNumber = 0;
+
+    //De spawnpoints toevoegen als array zodat ik random spawn points kan zetten.
+    GameObject[] spawnPoints;
+    GameObject currentPoint;
+    int index;
 
     public Text gameOverText;
     // Start is called before the first frame update
     void Start()
     {
+        //Spawnpoints zoeken doormiddel van tag
+        spawnPoints = GameObject.FindGameObjectsWithTag("spawnpoint");
+        //Random spawnpoint.
+        index = Random.Range(0, spawnPoints.Length);
+        //De huidige spawnpoint
+        currentPoint = spawnPoints[index];
         StartCoroutine(SpawnRound());
         gameOverText.gameObject.SetActive(false);
     }
@@ -25,11 +39,17 @@ public class Spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Constant zorgen dat de spawnpoint random is
+        index = Random.Range(0, spawnPoints.Length);
+        currentPoint = spawnPoints[index];
+
+        //Als er 0 zombies zijn dan start er een nieuwe ronde.
         if (GameObject.FindGameObjectsWithTag("zombie").Length == 0)
         {
             StartCoroutine(SpawnRound());
         }
 
+        //Als je bij ronde 10 bent finisht de game
         if (waveNumber == 10)
         {
             GameOver();
@@ -44,12 +64,17 @@ public class Spawner : MonoBehaviour
         //gameOverText.text == "Victory Royale";
     }
 
+    //Spawnround functie
     IEnumerator SpawnRound()    
     {
+        //Ronde gaat omhoog
         waveNumber++;
+        //Checkt of er geen zombies zijn dan spawnen er zombies.
         for (int i = 0; i < amountOfZombiesPerRound; i++)
         {
-            Instantiate(zombiePrefab, transform.position, Quaternion.identity);
+            //functie om de zombie te spawnen doormiddel van de prefab, rotatie en positie.
+            Instantiate(zombiePrefab, currentPoint.transform.position, Quaternion.identity);
+            //tijd tussen zombie spawn.
             yield return new WaitForSeconds(1f);
         }
     }
